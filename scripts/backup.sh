bf31273029509eec474f5479329e9dfaef4ab8a6
sota <furu.s1007@gmail.com>
2026-04-30 12:12:49 +0000

    バックアップ対象調整

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,3 +5,4 @@ mkdir -p /mnt/backup/daily/docker-volumes
 rsync -av --delete ~/homeserver/ /mnt/backup/daily/homeserver/
 rsync -av --delete /mnt/backup/vaultwarden/data/ /mnt/backup/daily/vaultwarden/
 sudo rsync -av --delete /var/lib/docker/volumes/ /mnt/backup/daily/docker-volumes/
+sudo cp /var/spool/cron/crontabs/sota /mnt/backup/daily/crontab-sota
